8feeed366093fa05ac1490596e35c41cc51801b4
ShrimpWin <moumoon711@gmai.com>
2022-05-26 15:39:02 +0800

    零跑

--- rewrite/leapmotor.js
+++ rewrite/leapmotor.js
@@ -3,7 +3,7 @@
 QX:
 [rewrite_local]
 #移除零跑开屏广告 要是有缓存过需要卸载APP重装
-^https:\/\/apptec.leapmotor.com\/appNewInterface\/getAppStartPageInfo url script-response-body https://raw.githubusercontent.com/ShrimpTang/QuanX/main/rewrite/leapmotor.js
+^https:\/\/apptec\.leapmotor\.com\/appNewInterface\/getAppStartPageInfo url script-response-body https://raw.githubusercontent.com/ShrimpTang/QuanX/main/rewrite/leapmotor.js
 
 [mitm]
 hostname = apptec.leapmotor.com
@@ -11,7 +11,7 @@ hostname = apptec.leapmotor.com
 */
 
 let obj = JSON.parse($response.body);
-obj.data.imgUrl ="";
-obj.data.contentUrl = "";
-obj.data.showTime = "0"
+obj.data[0].imgUrl ="";
+obj.data[0].contentUrl = "";
+obj.data[0].showTime = "0"
 $done({body: JSON.stringify(obj)});
